b154976f472128d0bb5f20b12b41ea055b0928a5
Otto Sievert <osievert@gopro.com>
2021-03-04 10:13:45 -0800

    feat: try5

--- src/utils.js
+++ src/utils.js
@@ -38,7 +38,7 @@ function buildSlackAttachments({ status, color, github }) {
           short: true,
         },
         {
-          title: 'StatuS',
+          title: 'Statusss',
           value: status,
           short: true,
         },
